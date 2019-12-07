803a638972a0c1f9a89de032d0eedd59b3d493d2
Romain Delamare <romain@delama.re>
2019-12-07 17:23:44 +0100

    day 5, part2

--- src/day5.rs
+++ src/day5.rs
@@ -1,47 +1,42 @@
 use crate::input::read_input;
-use im_rc::{HashMap, Vector};
+use im_rc::Vector;
+use std::cmp::Ordering;
 use std::iter::FromIterator;
 use std::ops::Index;
 
 pub fn execute() {
     let intcode: Intcode = read_input("day5")
         .split(",")
-        .map(|i| i.parse::<i32>().unwrap_or(0))
+        .map(|i| i.trim().parse::<i32>().unwrap())
         .collect();
-    let mut program = Program {
-        state: ProgramState {
-            intcode,
-            current_instruction: 0,
-            input: Input { value: 1 },
-            output: Output { value: None },
-        },
-        executors: HashMap::from(vec![
-            (ADD_OPCODE, ADD_EXECUTOR),
-            (MULTIPLY_OPCODE, MULTIPLY_EXECUTOR),
-            (INPUT_OPCODE, INPUT_EXECUTOR),
-            (OUTPUT_OPCODE, OUTPUT_EXECUTOR),
-        ]),
-    };
-    program.run();
-
-    println!("5:1 — Diagnostic code: {}", program.state.output.value.unwrap());
+    let program1 = run(ProgramState::new(&intcode, Input { value: 1 }));
+    let program2 = run(ProgramState::new(&intcode, Input { value: 5 }));
+    println!("5:1 — Diagnostic code: {}", program1.output.value.unwrap());
+    println!("5:2 — Diagnostic code: {}", program2.output.value.unwrap());
 }
 
-const ADD_OPCODE: i32 = 1;
-const MULTIPLY_OPCODE: i32 = 2;
-const INPUT_OPCODE: i32 = 3;
-const OUTPUT_OPCODE: i32 = 4;
 const END_OPCODE: i32 = 99;
 
-static INSTRUCTION_NB_PARAMETERS: [usize; 5] = [0, 3, 3, 1, 1];
+static INSTRUCTION_NB_PARAMETERS: [usize; 8] = [3, 3, 1, 1, 2, 2, 3, 3];
+static EXECUTORS: [OpcodeExecutor; 8] = [
+    ADD_EXECUTOR,
+    MULTIPLY_EXECUTOR,
+    INPUT_EXECUTOR,
+    OUTPUT_EXECUTOR,
+    JUMP_IF_TRUE_EXECUTOR,
+    JUMP_IF_FALSE_EXECUTOR,
+    LESS_THAN_EXECUTOR,
+    EQUALS_EXECUTOR,
+];
 
 static ADD_EXECUTOR: OpcodeExecutor = |state, parameters| {
     let parameter1_value = state.read_parameter(0, parameters[0]);
     let parameter2_value = state.read_parameter(1, parameters[1]);
     let result = parameter1_value + parameter2_value;
     let output_position = state.read_parameter(2, ParameterMode::Immediate);
+    let intcode = state.intcode.write(output_position as usize, result);
     ProgramState {
-        intcode: state.intcode.write(output_position as usize, result),
+        intcode,
         current_instruction: state.current_instruction + 4,
         input: state.input,
         output: state.output,
@@ -53,19 +48,21 @@ static MULTIPLY_EXECUTOR: OpcodeExecutor = |state, parameters| {
     let parameter2_value = state.read_parameter(1, parameters[1]);
     let result = parameter1_value * parameter2_value;
     let output_position = state.read_parameter(2, ParameterMode::Immediate);
+    let intcode = state.intcode.write(output_position as usize, result);
     ProgramState {
-        intcode: state.intcode.write(output_position as usize, result),
+        intcode,
         current_instruction: state.current_instruction + 4,
         input: state.input,
         output: state.output,
     }
 };
 
-static INPUT_EXECUTOR: OpcodeExecutor = |state, parameters| {
+static INPUT_EXECUTOR: OpcodeExecutor = |state, _parameters| {
     let result = state.input.read();
     let output_position = state.read_parameter(0, ParameterMode::Immediate);
+    let intcode = state.intcode.write(output_position as usize, result);
     ProgramState {
-        intcode: state.intcode.write(output_position as usize, result),
+        intcode,
         current_instruction: state.current_instruction + 2,
         input: state.input,
         output: state.output,
@@ -82,6 +79,78 @@ static OUTPUT_EXECUTOR: OpcodeExecutor = |state, parameters| {
         output,
     }
 };
+
+static JUMP_IF_TRUE_EXECUTOR: OpcodeExecutor = |state, parameters| {
+    let value = state.read_parameter(0, parameters[0]);
+    let new_instruction = match value {
+        0 => state.current_instruction + 3,
+        _ => state.read_parameter(1, parameters[1]) as usize,
+    };
+    ProgramState {
+        intcode: state.intcode.clone(),
+        current_instruction: new_instruction,
+        input: state.input,
+        output: state.output,
+    }
+};
+
+static JUMP_IF_FALSE_EXECUTOR: OpcodeExecutor = |state, parameters| {
+    let value = state.read_parameter(0, parameters[0]);
+    let new_instruction = match value {
+        0 => state.read_parameter(1, parameters[1]) as usize,
+        _ => state.current_instruction + 3,
+    };
+    ProgramState {
+        intcode: state.intcode.clone(),
+        current_instruction: new_instruction,
+        input: state.input,
+        output: state.output,
+    }
+};
+
+static LESS_THAN_EXECUTOR: OpcodeExecutor = |state, parameters| {
+    let parameter1_value = state.read_parameter(0, parameters[0]);
+    let parameter2_value = state.read_parameter(1, parameters[1]);
+    let output_position = state.read_parameter(2, ParameterMode::Immediate);
+    let output = match parameter1_value.cmp(&parameter2_value) {
+        Ordering::Less => 1,
+        _ => 0,
+    };
+    let intcode = state.intcode.write(output_position as usize, output);
+    ProgramState {
+        intcode,
+        current_instruction: state.current_instruction + 4,
+        input: state.input,
+        output: state.output,
+    }
+};
+
+static EQUALS_EXECUTOR: OpcodeExecutor = |state, parameters| {
+    let parameter1_value = state.read_parameter(0, parameters[0]);
+    let parameter2_value = state.read_parameter(1, parameters[1]);
+    let output_position = state.read_parameter(2, ParameterMode::Immediate);
+    let output = match parameter1_value.cmp(&parameter2_value) {
+        Ordering::Equal => 1,
+        _ => 0,
+    };
+    let intcode = state.intcode.write(output_position as usize, output);
+    ProgramState {
+        intcode,
+        current_instruction: state.current_instruction + 4,
+        input: state.input,
+        output: state.output,
+    }
+};
+
+fn run(state: ProgramState) -> ProgramState {
+    let mut current_state = state;
+    while !current_state.is_over() {
+        let instruction = current_state.get_next_instruction();
+        let executor = EXECUTORS[(&instruction.0 - 1) as usize];
+        current_state = executor(&current_state, &instruction.1);
+    }
+    current_state
+}
 
 #[derive(Copy, Clone)]
 struct Input {
@@ -100,14 +169,13 @@ struct Output {
 }
 
 impl Output {
+    fn new() -> Self {
+        Output { value: None }
+    }
+
     fn write(&self, out: i32) -> Output {
         Output { value: Some(out) }
     }
-}
-
-struct Program {
-    pub state: ProgramState,
-    executors: HashMap<i32, OpcodeExecutor>,
 }
 
 struct ProgramState {
@@ -117,7 +185,7 @@ struct ProgramState {
     output: Output,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct Intcode {
     code: Vector<i32>,
 }
@@ -130,17 +198,16 @@ enum ParameterMode {
     Immediate,
 }
 
-impl Program {
-    pub fn run(&mut self) {
-        while !self.state.is_over() {
-            let instruction = self.state.get_next_instruction();
-            let executor = self.executors[&instruction.0];
-            self.state = executor(&self.state, &instruction.1);
+impl ProgramState {
+    pub fn new(intcode: &Intcode, input: Input) -> Self {
+        ProgramState {
+            intcode: intcode.clone(),
+            current_instruction: 0,
+            input,
+            output: Output::new(),
         }
     }
-}
 
-impl ProgramState {
     pub fn is_over(&self) -> bool {
         self.instruction() == END_OPCODE
     }
@@ -183,6 +250,14 @@ impl FromIterator<i32> for Intcode {
     }
 }
 
+impl From<Vec<i32>> for Intcode {
+    fn from(vec: Vec<i32>) -> Self {
+        Intcode {
+            code: Vector::from(vec),
+        }
+    }
+}
+
 impl Index<usize> for Intcode {
     type Output = i32;
 
@@ -214,7 +289,9 @@ fn parse_parameters(instruction_parameters: i32) -> Vector<ParameterMode> {
 }
 
 fn fill_parameters(opcode: i32, parameters: &mut Vector<ParameterMode>) {
-    let nb_parameters = *INSTRUCTION_NB_PARAMETERS.get(opcode as usize).unwrap_or(&0);
+    let nb_parameters = *INSTRUCTION_NB_PARAMETERS
+        .get(opcode as usize - 1)
+        .unwrap_or(&0);
     while parameters.len() < nb_parameters {
         parameters.push_back(ParameterMode::Position)
     }
@@ -233,22 +310,16 @@ mod parse_instruction_should {
 
     #[test]
     fn return_opcode_and_position_mode_for_instruction_with_one_parameter_in_position_mode() {
-        let result = parse_instruction(INPUT_OPCODE);
+        let result = parse_instruction(3);
 
-        assert_eq!(
-            result,
-            (INPUT_OPCODE, Vector::from(vec![ParameterMode::Position]))
-        );
+        assert_eq!(result, (3, Vector::from(vec![ParameterMode::Position])));
     }
 
     #[test]
     fn return_opcode_and_immediate_mode_for_instruction_with_one_parameter_in_immediate_mode() {
         let result = parse_instruction(103);
 
-        assert_eq!(
-            result,
-            (INPUT_OPCODE, Vector::from(vec![ParameterMode::Immediate]))
-        );
+        assert_eq!(result, (3, Vector::from(vec![ParameterMode::Immediate])));
     }
 
     #[test]
@@ -258,7 +329,7 @@ mod parse_instruction_should {
         assert_eq!(
             result,
             (
-                MULTIPLY_OPCODE,
+                2,
                 Vector::from(vec![
                     ParameterMode::Position,
                     ParameterMode::Immediate,
@@ -266,5 +337,206 @@ mod parse_instruction_should {
                 ])
             )
         );
+    }
+}
+
+#[cfg(test)]
+mod run_tests {
+    use super::*;
+
+    #[test]
+    fn sample1_should_return_1_when_input_is_8() {
+        let program = ProgramState::new(
+            &Intcode::from(vec![3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8]),
+            Input { value: 8 },
+        );
+
+        let result = run(program);
+
+        assert_eq!(result.output.value, Some(1));
+    }
+
+    #[test]
+    fn sample1_should_return_0_when_input_is_not_8() {
+        let program = ProgramState::new(
+            &Intcode::from(vec![3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8]),
+            Input { value: 7 },
+        );
+
+        let result = run(program);
+
+        assert_eq!(result.output.value, Some(0));
+    }
+
+    #[test]
+    fn sample2_should_return_1_when_input_is_less_than_8() {
+        let program = ProgramState::new(
+            &Intcode::from(vec![3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8]),
+            Input { value: 5 },
+        );
+
+        let result = run(program);
+
+        assert_eq!(result.output.value, Some(1));
+    }
+
+    #[test]
+    fn sample2_should_return_0_when_input_is_8() {
+        let program = ProgramState::new(
+            &Intcode::from(vec![3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8]),
+            Input { value: 8 },
+        );
+
+        let result = run(program);
+
+        assert_eq!(result.output.value, Some(0));
+    }
+
+    #[test]
+    fn sample3_should_return_1_when_input_is_8() {
+        let program = ProgramState::new(
+            &Intcode::from(vec![3, 3, 1108, -1, 8, 3, 4, 3, 99]),
+            Input { value: 8 },
+        );
+
+        let result = run(program);
+
+        assert_eq!(result.output.value, Some(1));
+    }
+
+    #[test]
+    fn sample3_should_return_0_when_input_is_not_8() {
+        let program = ProgramState::new(
+            &Intcode::from(vec![3, 3, 1108, -1, 8, 3, 4, 3, 99]),
+            Input { value: 9 },
+        );
+
+        let result = run(program);
+
+        assert_eq!(result.output.value, Some(0));
+    }
+
+    #[test]
+    fn sample4_should_return_1_when_input_is_less_than_8() {
+        let program = ProgramState::new(
+            &Intcode::from(vec![3, 3, 1107, -1, 8, 3, 4, 3, 99]),
+            Input { value: 5 },
+        );
+
+        let result = run(program);
+
+        assert_eq!(result.output.value, Some(1));
+    }
+
+    #[test]
+    fn sample4_should_return_0_when_input_is_8() {
+        let program = ProgramState::new(
+            &Intcode::from(vec![3, 3, 1107, -1, 8, 3, 4, 3, 99]),
+            Input { value: 8 },
+        );
+
+        let result = run(program);
+
+        assert_eq!(result.output.value, Some(0));
+    }
+
+    #[test]
+    fn sample5_should_return_0_when_input_is_0() {
+        let program = ProgramState::new(
+            &Intcode::from(vec![
+                3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9,
+            ]),
+            Input { value: 0 },
+        );
+
+        let result = run(program);
+
+        assert_eq!(result.output.value, Some(0));
+    }
+
+    #[test]
+    fn sample5_should_return_1_when_input_is_not_0() {
+        let program = ProgramState::new(
+            &Intcode::from(vec![
+                3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9,
+            ]),
+            Input { value: 2 },
+        );
+
+        let result = run(program);
+
+        assert_eq!(result.output.value, Some(1));
+    }
+
+    #[test]
+    fn sample6_should_return_0_when_input_is_0() {
+        let program = ProgramState::new(
+            &Intcode::from(vec![3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1]),
+            Input { value: 0 },
+        );
+
+        let result = run(program);
+
+        assert_eq!(result.output.value, Some(0));
+    }
+
+    #[test]
+    fn sample6_should_return_1_when_input_is_not_0() {
+        let program = ProgramState::new(
+            &Intcode::from(vec![3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1]),
+            Input { value: 2 },
+        );
+
+        let result = run(program);
+
+        assert_eq!(result.output.value, Some(1));
+    }
+
+    #[test]
+    fn sample7_should_return_999_when_input_is_less_than_8() {
+        let program = ProgramState::new(
+            &Intcode::from(vec![
+                3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36,
+                98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46, 1101, 1000,
+                1, 20, 4, 20, 1105, 1, 46, 98, 99,
+            ]),
+            Input { value: 7 },
+        );
+
+        let result = run(program);
+
+        assert_eq!(result.output.value, Some(999));
+    }
+
+    #[test]
+    fn sample7_should_return_1000_when_input_is_8() {
+        let program = ProgramState::new(
+            &Intcode::from(vec![
+                3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36,
+                98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46, 1101, 1000,
+                1, 20, 4, 20, 1105, 1, 46, 98, 99,
+            ]),
+            Input { value: 8 },
+        );
+
+        let result = run(program);
+
+        assert_eq!(result.output.value, Some(1000));
+    }
+
+    #[test]
+    fn sample7_should_return_1001_when_input_is_more_than_8() {
+        let program = ProgramState::new(
+            &Intcode::from(vec![
+                3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36,
+                98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46, 1101, 1000,
+                1, 20, 4, 20, 1105, 1, 46, 98, 99,
+            ]),
+            Input { value: 9 },
+        );
+
+        let result = run(program);
+
+        assert_eq!(result.output.value, Some(1001));
     }
 }
